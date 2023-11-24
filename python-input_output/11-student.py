cad7d66e7bb58e2e0140c08c6f75e74dbb8c6a5a
ktanguy <t.kwizera@alustudent.com>
2023-11-24 12:36:15 -0800

    :)

--- python-input_output/11-student.py
+++ python-input_output/11-student.py
@@ -1,54 +1,32 @@
 #!/usr/bin/python3
-"""
-A Student class that defines a Studen (module)
-"""
+"""class_to_json"""
 
 
-<<<<<<< HEAD
 class Student:
-    """ 
-    this is a student class 
+    """Contains student data
     """
 
     def __init__(self, first_name, last_name, age):
-        """
-        just doing init for variables
-=======
-class Student():
-    """
-        A Student class that defines a Student
-    """
-    def __init__(self, first_name, last_name, age):
-        """
-            INIT
->>>>>>> 6427b0b5935bbdf2227a51d2669076a7153f394f
-        """
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
 
     def to_json(self, attrs=None):
-        """
-<<<<<<< HEAD
-        method converts to json 
-=======
-            retrieves a dictionary representation
-            of a Student instance
->>>>>>> 6427b0b5935bbdf2227a51d2669076a7153f394f
-        """
-        if type(attrs) is list and all([type(x) == str for x in attrs]):
-            return {k: v for k, v in self.__dict__.items() if k in attrs}
-        return(self.__dict__)
+        """class_to_json"""
+
+        if attrs is None or type(attrs) != list:
+            return self.__dict__
+        else:
+            temp = {}
+            for elem in attrs:
+                if type(elem) != str:
+                    return self.__dict__
+                if elem in self.__dict__.keys():
+                    temp[elem] = self.__dict__[elem]
+            return temp
 
     def reload_from_json(self, json):
-        """
-<<<<<<< HEAD
-        return json 
-        """
-        for i,j in json.items():
-            self.__dict__(i) = j
-=======
-        """
-        for i, j in json.items():
-            self.__dict__[i] = j
->>>>>>> 6427b0b5935bbdf2227a51d2669076a7153f394f
+        """reload_from_json"""
+
+        for items in json.keys():
+            self.__dict__[items] = json[items]
